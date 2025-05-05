c23e750ed7b6bb20d6fc64b3004d781bf94d0006
tin3ga <118301331+tin3ga@users.noreply.github.com>
2025-05-05 10:31:05 +0300

    update .gitignore, improve Dockerfile metadata, and refactor app.go for better variable handling and error management

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,18 @@
 ARG version=alpine
 FROM golang:${version} AS builder
+
 WORKDIR /app
 COPY app.go .
 RUN go build app.go
 
 
 FROM scratch
+LABEL org.opencontainers.image.authors="tin3ga" \
+      org.opencontainers.image.description="Container image for https://github.com/tin3ga/testEnv"
+
 COPY --from=builder /app/app .
 ENV NAME="Jane Doe"
 ENV CHARACTER="SpiderMan"
+ENV RUN_TIME="0"
 
 CMD ["./app"]
--- app.go
+++ app.go
@@ -3,12 +3,14 @@ package main
 import (
 	"fmt"
 	"os"
+	"strconv"
 	"time"
 )
 
 type Vars struct {
-	name      string
-	character string
+	Name      string
+	Character string
+	Seconds   string
 }
 
 func LoadVars() (*Vars, error) {
@@ -17,11 +19,25 @@ func LoadVars() (*Vars, error) {
 	// 	log.Fatal("Error loading .env file")
 	// }
 	vars := &Vars{
-		name:      os.Getenv("NAME"),
-		character: os.Getenv("CHARACTER"),
+		Name:      os.Getenv("NAME"),
+		Character: os.Getenv("CHARACTER"),
+		Seconds:   os.Getenv("RUN_TIME"),
 	}
 
 	return vars, nil
+}
+
+func ConvertToInt(str string) (int, error) {
+	if str == "" {
+		return -1, fmt.Errorf("empty string")
+	}
+	sec, err := strconv.Atoi(str)
+	if err != nil {
+		fmt.Println("Error converting seconds to int:", err)
+		os.Exit(1)
+	}
+	return sec, nil
+
 }
 
 func main() {
@@ -31,9 +47,30 @@ func main() {
 		os.Exit(1)
 	}
 
-	fmt.Printf("Hello,"+" %s"+"!", vars.name)
-	fmt.Printf("\nFav Character:"+" %s", vars.character)
+	if *vars == (Vars{}) {
+		vars = &Vars{
+			Name:      "arif",
+			Character: "octavia",
+			Seconds:   "0",
+		}
+	}
 
-	time.Sleep(20 * time.Minute)
+	fmt.Printf("Hello,"+" %s"+"!", vars.Name)
+	fmt.Printf("\nFav Character:"+" %s \n", vars.Character)
+
+	sec, err := ConvertToInt(vars.Seconds)
+	if err != nil || sec < 0 {
+		fmt.Println("Error converting seconds:", err)
+		os.Exit(1)
+	}
+
+	fmt.Printf("\rscript will run for %d seconds", sec)
+
+	for i := sec; i > 0; i-- {
+		time.Sleep(1 * time.Second)
+		fmt.Printf("\rscript will exit after %d seconds   ", i)
+	}
+	fmt.Printf("\rExited!                           \n")
+	os.Exit(0)
 
 }
